9df9cae332fb140604b80b563348b6090f37e42f
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-05-25 16:55:49 -0400

    viewDept function displays table of all departments and brings user back to start

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-//psuedocode:
-//add____ function
-//view ____ function
-//update employee roles function
-
-//const mysql = require("mysql");
 const inquirer = require("inquirer");
 const connection = require("./connection");
+require("console.table");
 
 function start(){
     inquirer.prompt({
-        name: "addViewUpdate",
+        name: "viewAddUpdate",
         type: "list",
-        message: "Would you like to Add or View depts, roles, or employees? Or update an employee's role?",
-        choices: ["ADD", "VIEW", "UPDATE", "EXIT"]
+        message: "Would you like to VIEW or ADD depts, roles, or employees? Or UPDATE an employee's role or manager?",
+        choices: [
+            "VIEW Departments",
+            "VIEW Roles",
+            "VIEW Employees",
+            "VIEW Employees by Role",
+            "VIEW Employees by Department",
+            "ADD Department",
+            "ADD Role",
+            "ADD Employee", 
+            "UPDATE Employee Role",
+            "UPDATE Employee Manager",
+            "EXIT"]
     }).then(answer => {
-        if(answer.addViewUpdate === "ADD") console.log("choice is ADD");
-        else if(answer.addViewUpdate === "VIEW") console.log("choice is VIEW");
-        else if(answer.addViewUpdate === "UPDATE") console.log("choice is UPDATE");
-        else connection.end();
+        if(answer.viewAddUpdate === "VIEW Departments") {
+            viewDept();
+        }else if(answer.viewAddUpdate === "ADD") {
+            console.log("choice is ADD");
+            //addDept();
+        }else if(answer.viewAddUpdate === "UPDATE") {
+            console.log("choice is UPDATE");
+
+        }else connection.end();
     });
 }
+
+function viewDept(){
+     connection.query("SELECT id, name FROM department", (err, result) => {
+         if (err) throw err;
+         console.table(result);
+         start();
+     });
+    };
+
 
 // function addDept(){
 //     inquirer.prompt({
@@ -27,7 +47,8 @@ function start(){
 //         type: "input",
 //         message: "What department would you like to add?"
 //     }).then(answer => {
-//         connection.query("INSERT")
-//     })
+//         connection.query("INSERT INTO department (name) SET ?", (answer.newDept));
+//         console.log(answer.newDept);
+//     });
 // }
-//  start();
+ start();
